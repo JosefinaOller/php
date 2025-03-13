74834b46465ffbd4091b94190736ebd49517244f
Josefina Oller <64923768+JosefinaOller@users.noreply.github.com>
2025-03-13 12:20:54 -0300

    Include vs Require

---
+++ ejercicios/includeVSrequire.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php require_once 'includeVSrequire_1.php';?>
+    <?php require_once 'includeVSrequire_1.php';?>
+    <?php echo "Hola";?>
+    <!--<?php include 'hola.php';?>-->
+    <!--<?php echo "Hola";?> -->
+</body>
+</html>
